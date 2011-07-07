6348ecd3f00fb00bfa06feb372e25241f3e46157
Aysar Khalid <ak@akpro.net>
2011-07-07 15:32:28 -0400

    Added kissmetrics integration code

--- application/views/template.php
+++ application/views/template.php
@@ -69,6 +69,16 @@ $.widget( "custom.catcomplete", $.ui.autocomplete, {
 		}
 	});
 	
+	//kissmetrics
+	var _kmq = _kmq || [];
+  function _kms(u){
+    setTimeout(function(){
+      var s = document.createElement('script'); var f = document.getElementsByTagName('script')[0]; s.type = 'text/javascript'; s.async = true;
+      s.src = u; f.parentNode.insertBefore(s, f);
+    }, 1);
+  }
+  _kms('//i.kissmetrics.com/i.js');_kms('//doug1izaerwt3.cloudfront.net/d8977a0cd5dace9f913e5f13bb972a0a61b72853.1.js');
+	
 $(function() {
 	$( "#mainSearchField" ).catcomplete({
 				source: "get_course_list.php",
