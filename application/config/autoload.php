72ffed3a00363a4b52d6c64969e7d7afee2b8fa4
Marinescu Alexandru <alex.marinescu90@gmail.com>
2011-07-31 13:24:10 +0300

    Added paypal library

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -52,7 +52,7 @@ $autoload['packages'] = array(APPPATH.'third_party');
 |	$autoload['libraries'] = array('database', 'session', 'xmlrpc');
 */
 
-$autoload['libraries'] = array('database','session','form_validation','ajax','table');
+$autoload['libraries'] = array('database','session','form_validation','ajax','table','paypal_lib');
 
 
 /*
