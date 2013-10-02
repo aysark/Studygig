4c180505b378116adf3cbd36acd7f3e831ce3d94
Alexandru Marinescu <alex.marinescu90@gmail.com>
2013-10-02 11:26:11 +0300

    Temp fix for membership not working after multiple payments

--- application/models/member.php
+++ application/models/member.php
@@ -1,10 +1,10 @@
 <?php
 
 class Member extends CI_Model {
-    
+
 	function add($user_id,$itemNum) {
-	
-		$new = array(	
+
+		$new = array(
 			'user_id' => $user_id,
 			'item_num' => $itemNum
 		);
@@ -17,14 +17,14 @@ class Member extends CI_Model {
 			case 3: $interval = 3;
 					break;
 			case 4: $interval = 1;
-					break;	
+					break;
 		}
 
 		$remove_subscription_query = "CREATE EVENT remove$user_id
 									  ON SCHEDULE AT CURRENT_TIMESTAMP + INTERVAL $interval MONTH
 									  DO
 									  	DELETE FROM members WHERE user_id = '$user_id'";
-	
+
 		$this->db->insert('members',$new);
 		$this->db->query($remove_subscription_query);
 	}
@@ -37,11 +37,11 @@ class Member extends CI_Model {
 	}
 
 	function is_member($id) {
-		
+
 		$this->db->where('user_id',$id);
 		$query = $this->db->get('members');
 
-		if ($query->num_rows() == 1)
+		if ($query->num_rows() != 0)
 			return TRUE;
 		else
 			return FALSE;
